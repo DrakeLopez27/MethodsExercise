45eeac0ecb94541ebab5fb43f9fd309ea0248880
DrakeLopez27 <121637335+DrakeLopez27@users.noreply.github.com>
2023-01-06 12:44:39 -0600

    methods

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hey, {userName}, what is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"Cool, {color} is my favorite too. What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"Wow, I love {animal}, what is your favorite band?");
+            var band = Console.ReadLine();
         }
     }
 }
